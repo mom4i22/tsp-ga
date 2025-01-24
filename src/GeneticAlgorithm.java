c87e4ebd3177855e333d316dd6399b4ef00e5067
Momchil Trenchev <daiparimaina@gmail.com>
2025-01-24 23:41:20 +0200

    Fix Euclidean distance formula

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -189,6 +189,6 @@ class Route {
     }
 
     private double distance(double x1, double y1, double x2, double y2) {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
